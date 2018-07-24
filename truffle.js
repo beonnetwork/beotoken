53dbfc6e73565ed968cd69df875d55748bd0e28b
Surat Teerapittayanon <steerapi@gmail.com>
2018-07-24 21:58:28 +0700

    Update truffle config

--- truffle.js
+++ truffle.js
@@ -5,40 +5,54 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
   networks: {
     mainnet: {
-      provider: new LedgerWalletProvider(
-        {
-          path: "44'/60'/0'/0", // ledger default derivation path
-          askConfirm: false,
-          accountsLength: 1,
-          networkId: 1, // mainnet
-          accountsOffset: 0, // we use the first address
-        },
-        `https://mainnet.infura.io/${process.env.INFURA_APIKEY}`,
-      ),
+      provider: function () {
+        return new LedgerWalletProvider({
+            path: "44'/60'/0'/0", // ledger default derivation path
+            askConfirm: false,
+            accountsLength: 1,
+            networkId: 1, // mainnet
+            accountsOffset: 0, // we use the first address
+          },
+          `https://mainnet.infura.io/${process.env.INFURA_APIKEY}`,
+        )
+      },
       network_id: 3,
-      gas: 4700000,
+      gas: 8000000,
     },
     ropsten: {
-      provider: new LedgerWalletProvider(
-        {
-          path: "44'/60'/0'/0", // ledger default derivation path
-          askConfirm: false,
-          accountsLength: 1,
-          networkId: 3, // ropsten testnet
-          accountsOffset: 0, // we use the first address
-        },
-        `https://ropsten.infura.io/${process.env.INFURA_APIKEY}`,
-      ),
+      provider: function () {
+        return new LedgerWalletProvider({
+            path: "44'/60'/0'/0", // ledger default derivation path
+            askConfirm: false,
+            accountsLength: 1,
+            networkId: 3, // ropsten testnet
+            accountsOffset: 0, // we use the first address
+          },
+          `https://ropsten.infura.io/${process.env.INFURA_APIKEY}`,
+        )
+      },
       network_id: 3,
       gas: 4700000,
     },
     ropstenhd: {
-      provider: new HDWalletProvider(
-        process.env.MNEMONIC,
-        `https://ropsten.infura.io/${process.env.INFURA_APIKEY}`,
-      ),
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/${process.env.INFURA_APIKEY}`,
+        )
+      },
       network_id: 3,
       gas: 4700000,
+    },
+    rinkebyhd: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://rinkeby.infura.io/${process.env.INFURA_APIKEY}`,
+        )
+      },
+      network_id: 4,
+      gas: 7000000,
     },
     development: {
       host: "localhost",
